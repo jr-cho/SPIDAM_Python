cdf8e099f19924ae04d0b9bef24ad943801a2f09
rboy2005 <richieruby66@gmail.com>
2024-11-18 14:18:21 -0500

    Creating comments as an outline for the GUI python file

---
+++ src/gui.py
@@ -0,0 +1,17 @@
+#importing necessary files
+
+#creating the tinker GUI
+
+#creating the settings for the title and size
+
+#creating the frame for the plot to be displayed it
+
+#creating introduction text and labels
+
+#creating a function to intake a file that is either a .mov or .wav
+
+#creating a function to display the plot when the plot button is clicked
+
+#creating buttons
+
+#running the GUI until the user closes it
