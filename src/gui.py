95fa57b7e4f252cab516c79d6c916b087cd2bc25
rboy2005 <richieruby66@gmail.com>
2024-11-18 14:26:20 -0500

    Creating the starter text, frame for the plot, and rhe setting for the GUI

--- src/gui.py
+++ src/gui.py
@@ -1,13 +1,23 @@
 #importing necessary files
-
+from tkinter import*
+from tkinter import filedialog
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 #creating the tinker GUI
-
+root = Tk()
 #creating the settings for the title and size
-
+root.title("SPIDAM Project")
+root.geometry('750x750')
 #creating the frame for the plot to be displayed it
-
+frame = Frame(root,width = 250, height = 250)
+frame.pack()
+frame.place(anchor = 'center', relx = 0.5,rely = 0.5)
 #creating introduction text and labels
-
+first_lbl = Label(root,text = "Thanks for using our program! To begin, please upload a .mov or .wav file")
+first_lbl.grid(row = 0, column = 0)
+file_lbl = Label(root, text = "Please upload a .mov or .wav file")
+file_lbl.place(x=100,y=31)
 #creating a function to intake a file that is either a .mov or .wav
 
 #creating a function to display the plot when the plot button is clicked
@@ -15,3 +25,4 @@
 #creating buttons
 
 #running the GUI until the user closes it
+root.mainloop()
