3f4381fcaf5d16e041da6d071238bb1af8435ed0
Jericho <joshmg426@gmail.com>
2024-12-04 01:35:33 -0500

    .

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).resolve().parent / "src"))
-
-from src.gui import create_gui  
-
-if __name__ == "__main__":
-    create_gui()
